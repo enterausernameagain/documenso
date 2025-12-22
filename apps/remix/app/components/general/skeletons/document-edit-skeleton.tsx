20c33ea2245790536e6d04e26d18143b43bd9178
Tim Lott Jr <timlottjr@tims-mbp.lan>
2025-12-22 17:15:22 -0500

    Revert "fix(jobs): fallback internal webapp url to public url"
    This reverts commit da1b1d8e9e1bffc132742a6d3274772505e1dbf0.

--- apps/remix/app/components/general/skeletons/document-edit-skeleton.tsx
+++ apps/remix/app/components/general/skeletons/document-edit-skeleton.tsx
@@ -7,7 +7,7 @@ import { Skeleton } from '@documenso/ui/primitives/skeleton';
 export default function DocumentEditSkeleton() {
   return (
     <div className="mx-auto -mt-4 flex w-full max-w-screen-xl flex-col px-4 md:px-8">
-      <Link to="/" className="flex grow-0 items-center text-[#2563EB] hover:opacity-80">
+      <Link to="/" className="flex grow-0 items-center text-[#7AC455] hover:opacity-80">
         <ChevronLeft className="mr-2 inline-block h-5 w-5" />
         <Trans>Documents</Trans>
       </Link>
@@ -21,17 +21,17 @@ export default function DocumentEditSkeleton() {
       </div>
 
       <div className="mt-4 grid h-[80vh] max-h-[60rem] w-full grid-cols-12 gap-x-8">
-        <div className="col-span-12 rounded-xl border-2 border-border bg-white/50 p-2 before:rounded-xl lg:col-span-6 xl:col-span-7 dark:bg-background">
+        <div className="dark:bg-background border-border col-span-12 rounded-xl border-2 bg-white/50 p-2 before:rounded-xl lg:col-span-6 xl:col-span-7">
           <div className="flex h-[80vh] max-h-[60rem] flex-col items-center justify-center">
-            <Loader className="h-12 w-12 animate-spin text-documenso" />
+            <Loader className="text-documenso h-12 w-12 animate-spin" />
 
-            <p className="mt-4 text-muted-foreground">
+            <p className="text-muted-foreground mt-4">
               <Trans>Loading document...</Trans>
             </p>
           </div>
         </div>
 
-        <div className="col-span-12 rounded-xl border-2 border-border bg-background before:rounded-xl lg:col-span-6 xl:col-span-5" />
+        <div className="bg-background border-border col-span-12 rounded-xl border-2 before:rounded-xl lg:col-span-6 xl:col-span-5" />
       </div>
     </div>
   );
